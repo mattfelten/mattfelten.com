0e29a31725ba0c0e1347acf8c7eb51f09317233a
Matt Felten <m@mattfelten.com>
2020-02-24 11:35:07 -0800

    Add store temp page so no 404

---
+++ src/pages/store.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SEO } from '../components';
+
+export const Store = () => (
+	<>
+		<SEO title="Store" />
+		<h1>Store</h1>
+		<p>Hmmm. How do I make a store now...</p>
+	</>
+);
+
+export default Store
