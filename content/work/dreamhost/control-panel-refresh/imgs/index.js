154595e41f097ec90e84c2510ded88e50225f890
Matt Felten <m@mattfelten.com>
2020-05-13 21:13:42 -0700

    Add remander of portfolio items

---
+++ content/work/dreamhost/control-panel-refresh/imgs/index.js
@@ -0,0 +1,29 @@
+import beforeAfter from './before-after.gif';
+import competitors from './competitors.png';
+import mobile from './mobile.gif';
+import navSketches2 from './nav.sketches.2.png';
+import navSketches from './nav.sketches.png';
+import panelIa from './panel.ia.png';
+import panelIcons from './panel.icons.png';
+import panelNav from './panel.nav.png';
+import panelSearch from './panel.search.png';
+import panelWebid from './panel.webid.png';
+import panelWireframes from './panel.wireframes.png';
+import panelWorkfile from './panel.workfile.png';
+import wireframeSketches from './wireframe.sketches.jpg';
+
+export {
+	beforeAfter,
+	competitors,
+	mobile,
+	navSketches2,
+	navSketches,
+	panelIa,
+	panelIcons,
+	panelNav,
+	panelSearch,
+	panelWebid,
+	panelWireframes,
+	panelWorkfile,
+	wireframeSketches
+}
