4c1409f8a833588fe1d413c1df0f767f5220b516
Matt Felten <m@mattfelten.com>
2021-01-04 18:33:37 -0800

    Get a nested dir example actually working

---
+++ pages/index.js
@@ -0,0 +1,74 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+
+import utilStyles from '../styles/utils.module.css';
+//import { getSortedPostsData } from '../lib/posts';
+//import { getFolderNestedData } from '../lib/docs';
+import { getSortedFlatData } from '../lib/docs';
+import Link from 'next/link';
+import Date from '../components/date';
+
+export async function getStaticProps() {
+  const allDocsFlatData = await getSortedFlatData('docs');
+
+  return {
+    props: {
+      allDocsFlatData,
+    },
+  };
+}
+
+export default function Home({
+  allDocsFlatData,
+}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <h2>NextJS Nested demo</h2>
+        <p>
+          This is a sample website to demonstrate nested markdown structure
+          rendered in next.js.
+        </p>
+        <p>
+          The code for the website can be found at:{' '}
+          <a
+            href='https://github.com/xypnox/next-nested'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            @xypnox/next-nested
+          </a>
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Docs - Flat</h2>
+        <p>
+          The Docs can also be displayed in a flattened manner as well but have
+          urls in same pattern as file structure. (These have not been sorted by
+          date)
+        </p>
+
+        <ul className={utilStyles.list}>
+          {allDocsFlatData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/docs/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+
+              {date && (
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              )}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
