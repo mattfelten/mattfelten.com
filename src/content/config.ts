ad26dfb1ff22630a4cf2c58f39f9f24ad4c68565
Matt Felten <m@mattfelten.com>
2024-08-13 14:39:34 -0700

    starting personal docs

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,24 +3,37 @@ import { defineCollection, z } from 'astro:content';
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
+const work = defineCollection({
+	schema: z.object({
+		company: z.string(),
+		description: z.string().optional(),
+		end: z.date().optional(),
+		role: z.string(),
+		start: z.date(),
+		website: z.string().optional(),
+	}),
+});
+
+const speaking = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		redirect: z.string(),
+		date: z.date(),
+	}),
+});
+
+const posts = defineCollection({});
+
+const docs = defineCollection({
+	schema: z.object({
+		title: z.string(),
+	}),
+});
+
 export const collections = {
-	work: defineCollection({
-		schema: z.object({
-			company: z.string(),
-			description: z.string().optional(),
-			end: z.date().optional(),
-			role: z.string(),
-			start: z.date(),
-			website: z.string().optional(),
-		}),
-	}),
-	speaking: defineCollection({
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			redirect: z.string(),
-			date: z.date(),
-		}),
-	}),
-	posts: defineCollection({}),
+	work,
+	speaking,
+	posts,
+	docs,
 };
