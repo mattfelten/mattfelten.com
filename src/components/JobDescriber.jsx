afe39a710a1d9fd58dae22126f38308a5bb4374c
Matt Felten <m@mattfelten.com>
2025-01-26 15:05:20 -0800

    redo intro paragraph. don't need to restate roles

---
+++ src/components/JobDescriber.jsx
@@ -0,0 +1,40 @@
+import { TypeAnimation } from 'react-type-animation';
+
+export const JobDescriber = ({
+	sequence,
+	delay = 6000,
+	speed = 20,
+	className,
+	shuffle = false,
+}) => {
+	var canUseDOM = !!(
+		typeof window !== 'undefined' &&
+		window.document &&
+		window.document.createElement
+	);
+
+	if (!canUseDOM) return sequence[0];
+
+	const shuffledSequence = shuffle ? shuffle(sequence) : sequence;
+
+	return (
+		<TypeAnimation
+			sequence={shuffledSequence.map((label, i) => [label, delay]).flat()}
+			wrapper="span"
+			preRenderFirstString={true}
+			cursor={true}
+			repeat={Infinity}
+			speed={speed}
+			className={`${className} after:text-accent after:opacity-70`}
+			style={{ display: 'inline-block' }}
+		/>
+	);
+};
+
+const shuffle = array => {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+};
