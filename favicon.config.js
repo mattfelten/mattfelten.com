cbe3663f5770208f243bcf72c095f1dc204a93b8
Matt Felten <m@mattfelten.com>
2025-01-26 23:52:33 -0800

    fix build

--- favicon.config.js
+++ favicon.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	input: './src/assets/favicon.svg', // Required. override cli options.
 	output: './public/favicons', // Required. override cli options.
-	template: './src/layout/favicon.html', // Default is the same as output, default name is meta.html.
+	template: './src/layouts/favicon.html', // Default is the same as output, default name is meta.html.
 	config: {
 		// The options are the same as https://github.com/itgalaxy/favicons#usage
 		path: '/favicons/',
